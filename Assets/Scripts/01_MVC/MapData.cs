7d345f4306b35ff50f41200453d3a4cd2b80984b
KyuSSeo <kyuri2665@gmail.com>
2025-12-09 15:20:59 +0900

    [Feat] MapData
    1. 폴더 정리
    2. 맵 데이터 스크립트  작성

---
+++ Assets/Scripts/01_MVC/MapData.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+    public List<Vector3> tiles;
+}
---
+++ Assets/Scripts/02_VMC/Tile.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    //  위치와 높이 정보
+    public Point pos;
+    public int height;
+    //  높이 정보 4단계
+    public const float stepHeight = 0.25f;
+
+    public Vector3 centor { get { return new Vector3(pos.x, height * stepHeight, pos.y); } }
+
+    //  타일 변형
+    public void Grow()
+    {
+        height++;
+        Match();
+    }
+
+    public void Shrink()
+
+    {
+        height--;
+        Match();
+    }
+
+    //  타일 정보 가져오기
+    public void Load(Point p, int h)
+    {
+        pos = p;
+        height = h;
+        Match();
+    }
+
+    public void Load(Vector3 v)
+    {
+        Load(new Point((int)v.x, (int)v.z), (int)v.y);
+    }
+
+    // 시각적 업데이트를 위한 함수
+    private void Match()
+    {
+        transform.localPosition = new Vector3(pos.x, height * stepHeight / 2f, pos.y);
+        transform.localScale = new Vector3(1, height * stepHeight, 1);
+    }
+}
