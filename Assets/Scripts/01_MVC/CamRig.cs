17694fed4a54aa3002856228e44afbad9ab75b85
KyuSSeo <kyuri2665@gmail.com>
2025-12-16 16:07:24 +0900

    [Chore] Camera shodo

--- Assets/Scripts/01_MVC/CamRig.cs
+++ Assets/Scripts/01_MVC/CamRig.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public class CamRig : MonoBehaviour
 {
     private Transform _transform;
-    public Transform follow;
+    public Transform target;
     public float speed = 3f;
+    public Vector3 targetOffset = Vector3.zero;
+
+    // 1. 턴이나 타일 선택을 기준오로 카메라의 기준점을 다시 설정합니다.
+    // 2. 마우스 좌클릭을 통해서 각도를 변경합니다.
+    // 3. 마우스 우클릭을 통해서 카메라 위치를 변경합니다.
+    // 4. 마우스 휠을 통해서 카메라의 거리를 변경합니다.
+
+    public bool isDrag = false;
+    
 
     void Awake()
     {
@@ -13,7 +22,7 @@ public class CamRig : MonoBehaviour
 
     void Update()
     {
-        if (follow)
-            _transform.position = Vector3.Lerp(_transform.position, follow.position, speed * Time.deltaTime);
+        if (target)
+            _transform.position = Vector3.Lerp(_transform.position, target.position, speed * Time.deltaTime);
     }
 }
