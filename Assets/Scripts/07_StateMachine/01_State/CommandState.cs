799f1d70360f4ac577c6934c4f5cbc5a94938689
KyuSSeo <kyuri2665@gmail.com>
2025-12-19 15:45:12 +0900

    [Feat] CommandState

--- Assets/Scripts/07_StateMachine/01_State/CommandState.cs
+++ Assets/Scripts/07_StateMachine/01_State/CommandState.cs
@@ -1,6 +1,69 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CommandState : TurnState
 {
-    
+    private AbilityMenuPanel menuPanel;
+
+    protected int index = 0;
+    protected List<string> options = new List<string>() { "Move", "Wait", "TurnEnd"};
+
+    public override void Enter()
+    {
+        base.Enter();
+
+
+        if (menuPanel == null)
+            menuPanel = FindAnyObjectByType<AbilityMenuPanel>();
+        menuPanel.Show("Action", options);
+        index = 0;
+        menuPanel.SetSelection(index);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        menuPanel.Hide();
+    }
+
+    protected override void OnMove(object sender, InfoEventArgs<Point> e)
+    {
+        if (e.info.y > 0)
+        {
+            index--;
+        }
+        else if (e.info.y < 0)
+        {
+            index++;
+        }
+
+        if (index < 0) index = options.Count - 1;
+        if (index >= options.Count) index = 0;
+
+        menuPanel.SetSelection(index);
+    }
+
+    protected override void OnFire(object sender, InfoEventArgs<int> e)
+    {
+        if (e.info == 0)
+        {
+            string choice = options[index];
+            switch (choice)
+            {
+                case "Move":
+                    owner.ChangeState<MoveToTargetState>(); 
+                    Debug.Log("Move Selected");
+                    break;
+                case "Wait":
+                    owner.ChangeState<SelectUnitState>();
+                    Debug.Log("Wait Selected");
+                    break;
+            }
+        }
+        else if (e.info == 1) 
+        {
+            owner.ChangeState<SelectUnitState>();
+            Debug.Log("Back");
+        }
+    }
 }
--- Assets/Scripts/09_UI/AbilityMenuPanel.cs
+++ Assets/Scripts/09_UI/AbilityMenuPanel.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEngine.UI;
 
 public class AbilityMenuPanel : MonoBehaviour
 {
     [Header("UI References")]
     public GameObject panelObject;
     public List<TextMeshProUGUI> menuOptions;
-    public Image selectionCursor;
 
     [Header("Settings")]
     public Color selectedColor = Color.yellow;
@@ -19,8 +15,6 @@ public class AbilityMenuPanel : MonoBehaviour
     public void Show(string title, List<string> options)
     {
         panelObject.SetActive(true);
-        // 옵션에 맞춰 버튼을 활성화하거나 텍스트를 세팅
-        // 간단히 고정된 메뉴 구현합니다.
     }
 
     public void Hide()
@@ -38,11 +32,6 @@ public class AbilityMenuPanel : MonoBehaviour
             if (i == index)
             {
                 menuOptions[i].color = selectedColor;
-                // 커서가 있다면 위치 이동
-                if (selectionCursor != null)
-                {
-                    selectionCursor.rectTransform.position = menuOptions[i].rectTransform.position;
-                }
             }
             else
             {
