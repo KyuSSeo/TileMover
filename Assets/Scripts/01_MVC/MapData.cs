0311f14de5f808a81ecae9f3300e98d5b555ecb0
KyuSSeo <kyuri2665@gmail.com>
2025-12-29 13:47:37 +0900

    [Feat] MapData SO add Spawn Point

--- Assets/Scripts/01_MVC/MapData.cs
+++ Assets/Scripts/01_MVC/MapData.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class MapData : ScriptableObject
 {
+    // 타일 정보 (좌표, 타일 타입)
     public List<Vector3> tiles;
+    // 타일 위 오브젝트 정보 ex) 지형지물
     public List<Vector3> units;
+    // 유닛 스폰지점 
+    public List<Vector3> spawnPoints;
 }
