58b67176444098c61206983fe6f7137e8ebbfd27
KyuSSeo <kyuri2665@gmail.com>
2025-12-29 14:16:55 +0900

    [Fix] Spawn same Tile
    같은 타잃에 스폰될 때 유닛 오브젝트 생성되는 현상 수정
    같은 타일에서 스폰을 완전 방지

--- Assets/Scripts/07_StateMachine/01_State/InitState.cs
+++ Assets/Scripts/07_StateMachine/01_State/InitState.cs
@@ -24,9 +24,6 @@ public class InitState : TurnState
     {
         for (int i = 0; i < mapData.spawnPoints.Count; i++)
         {
-            // 유닛 오브젝트 생성
-            GameObject instance = Instantiate(owner.charactor) as GameObject;
-
             // 좌표 설정
             int x = (int)mapData.spawnPoints[i].x;
             int z = (int)mapData.spawnPoints[i].z;
@@ -34,12 +31,15 @@ public class InitState : TurnState
             Tile targetTile = board.GetTile(pos);
 
             // 유닛 배치
-            Unit unit = instance.GetComponent<Unit>();
             if (targetTile == null || targetTile.content != null)
             {
                 Debug.LogWarning($"중복 배치 또는 잘못된 타일 ({x}, {z})");
                 continue;
             }
+            // 유닛 오브젝트 생성
+            GameObject instance = Instantiate(owner.charactor) as GameObject;
+
+            Unit unit = instance.GetComponent<Unit>();
             unit.Place(board.GetTile(pos));
             unit.DirMatch();
 
