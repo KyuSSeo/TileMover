b37bf4fe7c11a91b0b20ce0c91052c6414814e9c
KyuSSeo <kyuri2665@gmail.com>
2026-01-12 16:47:23 +0900

    [Feat] GameEnd Buttons

--- Assets/Scripts/08_Extensions/UI/ResultPanel.cs
+++ Assets/Scripts/08_Extensions/UI/ResultPanel.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ResultPanel
+public class ResultPanel : MonoBehaviour
 {
-    
+    [SerializeField] Button exit;
+    [SerializeField] Button restart;
+    [SerializeField] GameObject resultCanvus;
+
+    private TurnController turnController;
+
+    private void Awake()
+    {
+        ButtonsAddListener();
+    }
+    private void ButtonsAddListener()
+    {
+        exit.onClick.AddListener(OnClickExit);
+        restart.onClick.AddListener(OnClickRestart);
+    }
+    private void OnClickExit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    private void OnClickRestart()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
 }
